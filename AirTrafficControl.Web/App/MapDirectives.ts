c9fd49ce730384b00a57d7f95cd0130240d1f0b9
Karol Zadora-Przylecki <karolzadora@live.com>
2015-06-29 13:53:26 -0700

    Polyline is displayed on the map. Plus, update configuration to conform to latest Fabric tools.

--- AirTrafficControl.Web/App/MapDirectives.ts
+++ AirTrafficControl.Web/App/MapDirectives.ts
@@ -6,6 +6,7 @@
 module AirTrafficControl {
     (function () {
         var app = angular.module('AtcAppDirectives', []);
+        var seattleLocation = new Microsoft.Maps.Location(47.610, -122.232)
 
         app.directive('bingMap',(): ng.IDirective => {
 
@@ -24,20 +25,29 @@ module AirTrafficControl {
                     };
                     
                     scope.Map = new Microsoft.Maps.Map(instanceElement[0], {
-                        credentials: instanceAttributes["bing-maps-key"],
-                        zoom: 8,
+                        credentials: instanceAttributes["bingMapsKey"],
+                        zoom: 10,
                         disableZooming: true,
-                        center: new Microsoft.Maps.Location(47.610, -122.232)
+                        center: seattleLocation
                     });
+
+                    var options = { strokeColor: new Microsoft.Maps.Color(255, 0, 0, 255), strokeThickness: 3 };
+                    var polyline = new Microsoft.Maps.Polyline([
+                        new Microsoft.Maps.Location(seattleLocation.latitude - 0.1, seattleLocation.longitude - 0.1),
+                        new Microsoft.Maps.Location(seattleLocation.latitude + 0.1, seattleLocation.longitude - 0.1),
+                        new Microsoft.Maps.Location(seattleLocation.latitude + 0.1, seattleLocation.longitude),
+                        new Microsoft.Maps.Location(seattleLocation.latitude - 0.1, seattleLocation.longitude),
+                        new Microsoft.Maps.Location(seattleLocation.latitude - 0.1, seattleLocation.longitude + 0.1),
+                        new Microsoft.Maps.Location(seattleLocation.latitude + 0.1, seattleLocation.longitude + 0.1)],
+                        options);
+                    scope.Map.entities.push(polyline);
                     
                     scope.$watch("AirplaneStates",(newAirplaneStates, oldAirplaneStates, scope: IMainControllerScope) => {
-                        // TODO update airplane positions based on what is in scope.AirplaneStates
+                        
                         var map = scope.Map;
-                        map.entities.clear();
-                        var options = { strokeColor: new Microsoft.Maps.Color(0, 0, 255, Math.round(255 * Math.random())), strokeThickness: parseInt(thickness) };
-                        var polyline = new Microsoft.Maps.Polyline([new Microsoft.Maps.Location(latlon.latitude - 0.1, latlon.longitude - 0.1), new Microsoft.Maps.Location(latlon.latitude + 0.1, latlon.longitude - 0.1), new Microsoft.Maps.Location(latlon.latitude + 0.1, latlon.longitude), new Microsoft.Maps.Location(latlon.latitude - 0.1, latlon.longitude), new Microsoft.Maps.Location(latlon.latitude - 0.1, latlon.longitude + 0.1), new Microsoft.Maps.Location(latlon.latitude + 0.1, latlon.longitude + 0.1)], options);
-                        map.setView({ zoom: 10 });
-                        map.entities.push(polyline);
+                        // map.entities.clear();
+
+                        // TODO update airplane positions based on what is in scope.AirplaneStates
                     });
                 }
             };
