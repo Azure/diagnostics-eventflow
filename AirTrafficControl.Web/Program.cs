a129895e3e23dab093538a41d503682072e4b159
Karol Zadora-Przylecki <karolzadora@live.com>
2015-07-15 10:31:28 -0700

    Add ES listener to ATC and ATC.Web projects and move everything to .NET FX 4.5.1

--- AirTrafficControl.Web/Program.cs
+++ AirTrafficControl.Web/Program.cs
@@ -3,6 +3,8 @@ using System.Diagnostics;
 using System.Fabric;
 using System.Threading;
 
+using AirTrafficControl.SharedLib;
+
 namespace AirTrafficControl.Web
 {
     public class Program
@@ -13,6 +15,8 @@ namespace AirTrafficControl.Web
             {
                 using (FabricRuntime fabricRuntime = FabricRuntime.Create())
                 {
+                    ElasticSearchListener listener = new ElasticSearchListener(new Uri("http://kzelk03.cloudapp.net/es", UriKind.Absolute), "kzadora", "w13XP287!", "atc");
+
                     // This is the name of the ServiceType that is registered with FabricRuntime. 
                     // This name must match the name defined in the ServiceManifest. If you change
                     // this name, please change the name of the ServiceType in the ServiceManifest.
@@ -21,6 +25,8 @@ namespace AirTrafficControl.Web
                     ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(AirTrafficControlWeb).Name);
 
                     Thread.Sleep(Timeout.Infinite);
+
+                    GC.KeepAlive(listener);
                 }
             }
             catch (Exception e)
--- AirTrafficControl/Program.cs
+++ AirTrafficControl/Program.cs
@@ -4,6 +4,8 @@ using System.Fabric;
 using System.Threading;
 using Microsoft.ServiceFabric.Actors;
 
+using AirTrafficControl.SharedLib;
+
 namespace AirTrafficControl
 {
     public class Program
@@ -14,10 +16,14 @@ namespace AirTrafficControl
             {
                 using (FabricRuntime fabricRuntime = FabricRuntime.Create())
                 {
+                    ElasticSearchListener listener = new ElasticSearchListener(new Uri("http://kzelk03.cloudapp.net/es", UriKind.Absolute), "kzadora", "w13XP287!", "atc");
+                    
                     fabricRuntime.RegisterActor(typeof(AirTrafficControl));
                     fabricRuntime.RegisterActor(typeof(Airplane));
 
                     Thread.Sleep(Timeout.Infinite);
+
+                    GC.KeepAlive(listener);
                 }
             }
             catch (Exception e)
