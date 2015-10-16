2452f18e6545f4b8ae5c1594eef0b8e3d67e35c3
Karol Zadora-Przylecki <kzadora@live.com>
2015-10-16 09:56:50 -0700

    Add missing file

---
+++ AirTrafficControl.Web/Fabric/AirTrafficControlWeb.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.ServiceFabric.Services;
+using Nancy.TinyIoc;
+using System.Fabric;
+
+namespace AirTrafficControl.Web.Fabric
+{
+    public class AirTrafficControlWeb : StatelessService
+    {
+        protected override ICommunicationListener CreateCommunicationListener()
+        {
+            var listener = new OwinCommunicationListener(new OwinStartup());
+            listener.ServiceInitialized += OnServiceInitialized;
+            return listener;
+        }
+
+        private void OnServiceInitialized(object sender, ServiceInitializedEventArgs e)
+        {
+            if (e.InitializationParameters is StatefulServiceInitializationParameters)
+            {
+                StatefulServiceInitializationParameters statefulInitParams = (StatefulServiceInitializationParameters)e.InitializationParameters;
+                var fabricContext = new FabricContext<StatefulServiceInitializationParameters>(statefulInitParams);
+                TinyIoCContainer.Current.Register<FabricContext<StatefulServiceInitializationParameters>>(fabricContext).AsSingleton();
+            }
+            else if (e.InitializationParameters is StatelessServiceInitializationParameters)
+            {
+                StatelessServiceInitializationParameters statelessInitParams = (StatelessServiceInitializationParameters)e.InitializationParameters;
+                var fabricContext = new FabricContext<StatelessServiceInitializationParameters>(statelessInitParams);
+                TinyIoCContainer.Current.Register<FabricContext<StatelessServiceInitializationParameters>>(fabricContext).AsSingleton();
+            }
+        }
+    }
+}
