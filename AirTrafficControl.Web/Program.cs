d218749adef6cb50f7c3a25e13dbacbaa21f9954
Karol Zadora-Przylecki <kzadora@live.com>
2016-04-08 10:57:30 -0700

    Services now start, but ATC cannot send notifications

--- AirTrafficControl.Web/Program.cs
+++ AirTrafficControl.Web/Program.cs
@@ -1,6 +1,6 @@
 ﻿using AirTrafficControl.Web.Fabric;
 using Microsoft.Diagnostics.EventListeners;
-
+using Microsoft.ServiceFabric.Services.Runtime;
 using System;
 using System.Configuration;
 using System.Diagnostics;
@@ -17,27 +17,24 @@ namespace AirTrafficControl.Web
         {
             try
             {
-                using (FabricRuntime fabricRuntime = FabricRuntime.Create())
+                const string ElasticSearchEventListenerId = "ElasticSearchEventListener";
+                FabricEventListeners.FabricConfigurationProvider configProvider = new FabricEventListeners.FabricConfigurationProvider(ElasticSearchEventListenerId);
+                ElasticSearchListener listener = null;
+                if (configProvider.HasConfiguration)
                 {
-                    const string ElasticSearchEventListenerId = "ElasticSearchEventListener";
-                    FabricEventListeners.FabricConfigurationProvider configProvider = new FabricEventListeners.FabricConfigurationProvider(ElasticSearchEventListenerId);
-                    ElasticSearchListener listener = null;
-                    if (configProvider.HasConfiguration)
-                    {
-                        listener = new ElasticSearchListener(configProvider, new FabricEventListeners.FabricHealthReporter(ElasticSearchEventListenerId));
-                    }
-
-                    // This is the name of the ServiceType that is registered with FabricRuntime. 
-                    // This name must match the name defined in the ServiceManifest. If you change
-                    // this name, please change the name of the ServiceType in the ServiceManifest.
-                    fabricRuntime.RegisterServiceType("AirTrafficControlWebType", typeof(AirTrafficControlWeb));
-
-                    ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(AirTrafficControlWeb).Name);
-
-                    Thread.Sleep(Timeout.Infinite);
-
-                    GC.KeepAlive(listener);
+                    listener = new ElasticSearchListener(configProvider, new FabricEventListeners.FabricHealthReporter(ElasticSearchEventListenerId));
                 }
+
+                // This is the name of the ServiceType that is registered with FabricRuntime. 
+                // This name must match the name defined in the ServiceManifest. If you change
+                // this name, please change the name of the ServiceType in the ServiceManifest.
+                ServiceRuntime.RegisterServiceAsync("AirTrafficControlWebType", ctx => new AirTrafficControlWeb(ctx)).Wait();
+
+                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(AirTrafficControlWeb).Name);
+
+                Thread.Sleep(Timeout.Infinite);
+
+                GC.KeepAlive(listener);
             }
             catch (Exception e)
             {
