c872ef1bbc4497c64ecbcc5147c4c176e20e609a
Karol Zadora-Przylecki <kzadora@live.com>
2016-04-26 10:00:12 -0700

    Fix a bug where EnrouteClearance loses the FlightPath property

--- AirTrafficControl.Interfaces/AtcInstruction.cs
+++ AirTrafficControl.Interfaces/AtcInstruction.cs
@@ -61,12 +61,13 @@ namespace AirTrafficControl.Interfaces
     [DataContract]
     public class EnrouteClearance: AtcInstruction
     {
-        private IList<Fix> flightPath;
+        [DataMember]
+        public IList<Fix> FlightPath { get; private set; }
 
         public EnrouteClearance(Fix limit, IList<Fix> flightPath) : base(limit)
         {
             Requires.NotNull(flightPath, nameof(flightPath));
-            this.flightPath = flightPath;
+            this.FlightPath = flightPath;
             Requires.ValidState(flightPath.Contains(limit), "The flight path must contain the clearance limit");
         }
 
@@ -79,13 +80,13 @@ namespace AirTrafficControl.Interfaces
         {
             Requires.NotNull(current, nameof(current));
             Requires.NotNull(target, nameof(target));
-            Requires.That(this.flightPath.Contains(current), nameof(current), "The 'current' fix is not part of the flight path");
-            Requires.That(this.flightPath.Contains(target), nameof(target), "The 'target' fix is not part of the flight path");
+            Requires.That(this.FlightPath.Contains(current), nameof(current), "The 'current' fix is not part of the flight path");
+            Requires.That(this.FlightPath.Contains(target), nameof(target), "The 'target' fix is not part of the flight path");
             Assumes.False(current == target, "Current fix cannot be the same as the target fix");
 
-            int limitIndex = this.flightPath.IndexOf(this.LocationOrLimit);
-            int currentIndex = this.flightPath.IndexOf(current);
-            int targetIndex = this.flightPath.IndexOf(target);
+            int limitIndex = this.FlightPath.IndexOf(this.LocationOrLimit);
+            int currentIndex = this.FlightPath.IndexOf(current);
+            int targetIndex = this.FlightPath.IndexOf(target);
 
             bool sameDirection = currentIndex < targetIndex;
             bool notGoingBeyondTheLimit = targetIndex <= limitIndex;
