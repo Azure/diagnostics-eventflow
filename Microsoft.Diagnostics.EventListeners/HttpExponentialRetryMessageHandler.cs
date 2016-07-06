10b6cc4fd2aee595aee16e0e3b88fe9d4e5a9f16
Karol Zadora-Przylecki <karolz@microsoft.com>
2016-07-06 16:12:24 -0700

    Preserve work on OmsEventListener

---
+++ Microsoft.Diagnostics.EventListeners/HttpExponentialRetryMessageHandler.cs
@@ -0,0 +1,38 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using Polly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Diagnostics.EventListeners
+{
+    public class HttpExponentialRetryMessageHandler: DelegatingHandler
+    {
+        private Polly.Retry.RetryPolicy<HttpResponseMessage> retryPolicy;
+        private Polly.CircuitBreaker.CircuitBreakerPolicy<HttpResponseMessage> breakerPolicy;
+
+        public HttpExponentialRetryMessageHandler(): base(new HttpClientHandler())
+        {
+            this.retryPolicy = Policy
+                .HandleResult<HttpResponseMessage>(responseMessage => ((int)responseMessage.StatusCode) >= 500)
+                .WaitAndRetryAsync(new[] { TimeSpan.FromMilliseconds(500), TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(5)});
+
+            this.breakerPolicy = Policy
+                .HandleResult<HttpResponseMessage>(responseMessage => ((int)responseMessage.StatusCode) >= 500)
+                .CircuitBreakerAsync(handledEventsAllowedBeforeBreaking: 3, durationOfBreak: TimeSpan.FromMinutes(2));
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return this.breakerPolicy.ExecuteAsync(() => this.retryPolicy.ExecuteAsync(() => base.SendAsync(request, cancellationToken)));
+        }
+    }
+}
--- Microsoft.Diagnostics.EventListeners/OmsEventListener.cs
+++ Microsoft.Diagnostics.EventListeners/OmsEventListener.cs
@@ -6,6 +6,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,12 +16,16 @@ namespace Microsoft.Diagnostics.EventListeners
 {
     public class OmsEventListener : BufferingEventListener, IDisposable
     {
+        private HttpExponentialRetryMessageHandler retryHandler;
+
         public OmsEventListener(IConfigurationProvider configurationProvider, IHealthReporter healthReporter) : base(configurationProvider, healthReporter)
         {
             if (this.Disabled)
             {
                 return;
             }
+
+            this.retryHandler = new HttpExponentialRetryMessageHandler();
 
             this.Sender = new ConcurrentEventSender<EventData>(
                 eventBufferSize: 1000,
@@ -32,7 +38,36 @@ namespace Microsoft.Diagnostics.EventListeners
 
         private async Task SendEventsAsync(IEnumerable<EventData> events, long transmissionSequenceNumber, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            string workspaceId = "30787dd1-cacc-4bca-8944-ba88ccb6f350";
+            string url = "/api/logs?api-version=2016-04-01";
+
+            try
+            {
+                // TODO: the client can be created in advance
+                using (var client = new HttpClient(retryHandler))
+                {
+                    client.BaseAddress = new Uri($"https://{workspaceId}.ods.opinsights.azure.com", UriKind.Absolute);
+                    client.DefaultRequestHeaders.Add("Log-Type", "TestLFALogs");
+
+                    HttpContent content = new StringContent("blah blah", System.Text.Encoding.UTF8, "application/json");
+                    content.Headers.Add("Authorization", "signature goes here");
+                    content.Headers.Add("x-ms-date", "date goes here");
+
+                    HttpResponseMessage response = await client.PostAsync(url, null, cancellationToken);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        this.ReportListenerHealthy();
+                    }
+                    else
+                    {
+                        this.ReportListenerProblem($"OMS REST API returned an error. Code: {response.StatusCode} Description: ${response.ReasonPhrase}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                this.ReportListenerProblem($"An error occurred while sending data to OMS: {e.ToString()}");
+            }
         }
     }
 }
