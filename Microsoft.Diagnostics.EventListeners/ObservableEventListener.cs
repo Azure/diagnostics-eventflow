c66853b044dda8867ecc87b01df5561e314907c1
Karol Zadora-Przylecki <karolz@microsoft.com>
2016-07-26 16:21:00 -0700

    Preserve work

---
+++ Microsoft.Diagnostics.EventListeners/ObservableEventListener.cs
@@ -0,0 +1,21 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Diagnostics.EventListeners
+{
+    public class ObservableEventListener : IObservable<EventData>
+    {
+        public IDisposable Subscribe(IObserver<EventData> observer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Microsoft.Diagnostics.EventListeners/SimpleSubject.cs
@@ -0,0 +1,149 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Validation;
+
+namespace Microsoft.Diagnostics.EventListeners
+{
+    internal class SimpleSubject<SubjectType> : IObservable<SubjectType>, IObserver<SubjectType>, IDisposable
+    {
+        private readonly object lockObject = new object();
+        private volatile ImmutableList<IObserver<SubjectType>> observers = ImmutableList<IObserver<SubjectType>>.Empty;
+        private volatile bool notifyingObservers = false;
+
+        public void Dispose()
+        {
+            this.OnCompleted();
+        }
+
+        public void OnCompleted()
+        {
+            IEnumerable<IObserver<SubjectType>> remainingObservers = Shutdown();
+            if (remainingObservers != null)
+            {
+                Parallel.ForEach(remainingObservers, observer => observer.OnCompleted());
+            }
+        }
+
+        public void OnError(Exception error)
+        {
+            IEnumerable<IObserver<SubjectType>> remainingObservers = Shutdown();
+            if (remainingObservers != null)
+            {
+                Parallel.ForEach(remainingObservers, observer => observer.OnError(error));
+            }
+        }
+
+        public void OnNext(SubjectType value)
+        {
+            var currentObservers = this.observers;
+            if (currentObservers != null)
+            {
+                try
+                {
+                    this.notifyingObservers = true;
+
+                    foreach (var observer in currentObservers)
+                    {
+                        // In our library we expect observers to process notifications very fast and there is no point in notifying them in parallel.
+                        // We also do not really care whether we invoke OnNext in parallel on our observers, as they are resilient to that.
+                        observer.OnNext(value);
+                    }
+                }
+                finally
+                {
+                    this.notifyingObservers = false;
+                }
+            }
+        }
+
+        public IDisposable Subscribe(IObserver<SubjectType> observer)
+        {
+            Requires.NotNull(observer, nameof(observer));
+            lock(this.lockObject)
+            {
+                if (IsCompleted)
+                {
+                    observer.OnCompleted();
+                    return new EmptyDisposable();
+                }
+                else
+                {
+                    this.observers = this.observers.Add(observer);
+                    return new Subscription<SubjectType>(observer, this);
+                }
+            }
+        }
+
+        public void Unsubscribe(IObserver<SubjectType> observer)
+        {
+            lock(this.lockObject)
+            {
+                if (!IsCompleted)
+                {
+                    this.observers = this.observers.Remove(observer);
+                }                
+            }
+        }
+
+        private bool IsCompleted
+        {
+            get { return this.observers == null; }
+        }
+
+        private IEnumerable<IObserver<SubjectType>> Shutdown()
+        {
+            IEnumerable<IObserver<SubjectType>> currentObservers;
+            lock (this.lockObject)
+            {
+                currentObservers = this.observers;
+                this.observers = null;                
+            }
+
+            // Wait for observer notifications to complete before completing them (i.e. calling OnCompleted or OnError).
+            SpinWait.SpinUntil(() => !this.notifyingObservers, TimeSpan.FromSeconds(5));
+            return currentObservers;
+        }
+
+        private class Subscription<SubscriptionType> : IDisposable
+        {
+            private IObserver<SubscriptionType> observer;
+            private SimpleSubject<SubscriptionType> parentSubject;
+
+            public Subscription(IObserver<SubscriptionType> observer, SimpleSubject<SubscriptionType> parentSubject)
+            {
+                Debug.Assert(observer != null);
+                Debug.Assert(parentSubject != null);
+                this.parentSubject = parentSubject;
+                this.observer = observer;
+            }
+
+            public void Dispose()
+            {
+                var current = Interlocked.Exchange<IObserver<SubscriptionType>>(ref this.observer, null);
+                if (current != null)
+                {
+                    this.parentSubject.Unsubscribe(current);
+                    this.parentSubject = null;
+                }
+            }
+        }
+
+        private class EmptyDisposable : IDisposable
+        {
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
