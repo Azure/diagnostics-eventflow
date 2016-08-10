fcdf94c17bd72931b1f6043a14910ae2cddb06ae
Saar Shen <xiaomi7732@hotmail.com>
2016-08-10 16:26:56 -0700

    Convert core to .NET Core + NETFx45

---
+++ Microsoft.Extensions.Diagnostics.Common/Interfaces/IEventFilter.cs
@@ -0,0 +1,18 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.Diagnostics
+{
+    public interface IEventFilter<EventDataType>
+    {
+        bool Filter(EventDataType eventData);
+    }
+}
---
+++ Microsoft.Extensions.Diagnostics.Common/Interfaces/IEventSender.cs
@@ -0,0 +1,16 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.Diagnostics
+{
+    public interface IEventSender<EventDataType>
+    {
+        Task SendEventsAsync(IReadOnlyCollection<EventDataType> events, long transmissionSequenceNumber, CancellationToken cancellationToken);
+    }
+}
