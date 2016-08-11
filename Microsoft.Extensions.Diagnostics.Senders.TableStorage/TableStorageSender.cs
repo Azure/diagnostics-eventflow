d6fe16560f486ca640c8102df9a8640f32cb7203
Saar Shen <xiaomi7732@hotmail.com>
2016-08-11 12:31:56 -0700

    Convert table storage sender

--- Microsoft.Extensions.Diagnostics.Senders.TableStorage/TableStorageSender.cs
+++ Microsoft.Extensions.Diagnostics.Senders.TableStorage/TableStorageSender.cs
@@ -2,21 +2,20 @@
 //  Copyright (c) Microsoft Corporation.  All rights reserved.
 //  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
 // ------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using Validation;
 
 namespace Microsoft.Extensions.Diagnostics
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Configuration;
-    using System.Diagnostics;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using Extensions.Configuration;
-    using Microsoft.WindowsAzure.Storage;
-    using Microsoft.WindowsAzure.Storage.Auth;
-    using Microsoft.WindowsAzure.Storage.Table;
-    using Validation;
-
     public class TableStorageSender : EventDataSender
     {
         private const int MaxConcurrentPartitions = 4;
