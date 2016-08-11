47c1c5b9266760f14feac0471e83bdc3fd99485b
Saar Shen <xiaomi7732@hotmail.com>
2016-08-11 12:23:27 -0700

    Convert Oms event sender

--- Microsoft.Extensions.Diagnostics.Common/Implementations/HttpExponentialRetryMessageHandler.cs
+++ Microsoft.Extensions.Diagnostics.Common/Implementations/HttpExponentialRetryMessageHandler.cs
@@ -4,10 +4,7 @@
 // ------------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
