fe5e7b9685cfe9c01d311db93d5143d2c44463c9
Saar Shen <xiaomi7732@hotmail.com>
2016-08-23 21:42:46 -0700

    Append restore for FilterParserGen

--- tools/restore.cmd
+++ tools/restore.cmd
@@ -1,2 +1,5 @@
 @ECHO OFF
 dotnet restore %~dp0..\
+pushd ..
+tools\nuget.exe restore "src\Microsoft.Extensions.Diagnostics.FilterParserGenerator\Microsoft.Extensions.Diagnostics.FilterParserGenerator.csproj" -PackagesDirectory packages
+popd
