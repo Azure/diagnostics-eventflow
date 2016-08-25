a9bc96c03579d320d5c9f87c5f185d85a0dcb318
yantang <yantang@microsoft.com>
2016-08-25 16:48:31 -0700

    A minor modification of setEnv.cmd

--- tools/setEnv.cmd
+++ tools/setEnv.cmd
@@ -1,7 +1,10 @@
 @ECHO OFF
- 
+pushd %~dp0
+
 where dotnet >NUL 2>NUL
 IF %ERRORLEVEL% NEQ 0 powershell.exe -File InstallCoreSDK.ps1
 
 ECHO %PATH% | findstr /I /C:"C:\Program Files\dotnet" >NUL 2>NUL
 IF %ERRORLEVEL% NEQ 0 SET PATH=%PATH%;C:\Program Files\dotnet\
+
+popd
