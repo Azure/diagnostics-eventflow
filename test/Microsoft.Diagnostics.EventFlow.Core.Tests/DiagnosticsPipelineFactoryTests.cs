ade3216bb4fa8a4e09f065709af8817f4699df3c
yantang <yantang@microsoft.com>
2016-08-30 03:24:13 -0700

    Change pipeline factory signature, so it only takes the config file or IConfiguration

--- test/Microsoft.Diagnostics.EventFlow.Core.Tests/DiagnosticsPipelineFactoryTests.cs
+++ test/Microsoft.Diagnostics.EventFlow.Core.Tests/DiagnosticsPipelineFactoryTests.cs
@@ -3,13 +3,16 @@
 //  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
 // ------------------------------------------------------------
 
+using System.IO;
 using System.Linq;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Diagnostics.EventFlow.Configuration;
 using Microsoft.Diagnostics.EventFlow.Filters;
+using Microsoft.Diagnostics.EventFlow.HealthReporters;
 using Microsoft.Diagnostics.EventFlow.Inputs;
 using Microsoft.Diagnostics.EventFlow.Metadata;
 using Microsoft.Diagnostics.EventFlow.Outputs;
+using Microsoft.Diagnostics.EventFlow.Tests.Mocks;
+using Microsoft.Extensions.Configuration;
 using Moq;
 using Xunit;
 
@@ -17,6 +20,99 @@ namespace Microsoft.Diagnostics.EventFlow.Tests
 {
     public class DiagnosticsPipelineFactoryTests
     {
+        [Fact]
+        public void ShouldUseDefaultHealthReporterIfNotSpecified()
+        {
+            string pipelineConfiguration = @"
+                {
+                    ""inputs"": [
+                        {
+                            ""type"": ""EventSource"",
+                            ""sources"": [
+                                { ""providerName"": ""Microsoft-ServiceFabric-Services"" },
+                            ]
+                        }
+                    ],
+                    ""outputs"": [
+                        {
+                            ""type"": ""StdOutput"",
+                        }
+                    ],
+                    ""schema-version"": ""2016-08-11"",
+                }";
+
+            try
+            {
+                using (var configFile = new TemporaryFile())
+                {
+                    configFile.Write(pipelineConfiguration);
+                    ConfigurationBuilder builder = new ConfigurationBuilder();
+                    builder.AddJsonFile(configFile.FilePath);
+                    var configuration = builder.Build();
+
+                    using (var pipeline = DiagnosticsPipelineFactory.CreatePipeline(configuration))
+                    {
+                        Assert.NotNull(pipeline);
+                        Assert.True(pipeline.HealthReporter is CsvHealthReporter);
+                    }
+                }
+            }
+            finally
+            {
+                TryDeleteFile(CsvHealthReporter.DefaultHealthReportName);
+            }
+
+        }
+
+        [Fact]
+        public void ShouldUse3rdPartyHealthReporterIfSpecified()
+        {
+            string pipelineConfiguration = @"
+                {
+                    ""inputs"": [
+                        {
+                            ""type"": ""EventSource"",
+                            ""sources"": [
+                                { ""providerName"": ""Microsoft-ServiceFabric-Services"" },
+                            ]
+                        }
+                    ],
+                    ""outputs"": [
+                        {
+                            ""type"": ""StdOutput"",
+                        }
+                    ],
+                    ""schema-version"": ""2016-08-11"",
+
+                    ""extensions"": [
+                        {
+                            ""category"": ""healthReporter"",
+                            ""type"": ""HealthReporterMock"",
+                            ""assemblyQualifiedName"": ""Microsoft.Diagnostics.EventFlow.Tests.Mocks.HealthReporterMock, Microsoft.Diagnostics.EventFlow.Core.Tests""
+                        }
+                    ],
+                    ""healthReporter"": {
+                        ""type"": ""HealthReporterMock"",
+                    }
+                }";
+
+            using (var configFile = new TemporaryFile())
+            {
+                configFile.Write(pipelineConfiguration);
+                ConfigurationBuilder builder = new ConfigurationBuilder();
+                builder.AddJsonFile(configFile.FilePath);
+                var configuration = builder.Build();
+
+                var mocked = new Mock<IHealthReporter>();
+                var isHP = mocked is IHealthReporter;
+                using (var pipeline = DiagnosticsPipelineFactory.CreatePipeline(configuration))
+                {
+                    Assert.NotNull(pipeline);
+                    Assert.True(pipeline.HealthReporter is HealthReporterMock);
+                }
+            }
+        }
+
         [Fact]
         public void ShouldInstantiatePipelineFromValidConfiguration()
         {
@@ -59,49 +155,62 @@ namespace Microsoft.Diagnostics.EventFlow.Tests
                     ""schema-version"": ""2016-08-11"",
                 }";
 
-            using (var configFile = new TemporaryFile())
+            try
             {
-                configFile.Write(pipelineConfiguration);
-                ConfigurationBuilder builder = new ConfigurationBuilder();
-                builder.AddJsonFile(configFile.FilePath);
-                var configuration = builder.Build();
+                using (var configFile = new TemporaryFile())
+                {
+                    configFile.Write(pipelineConfiguration);
+                    ConfigurationBuilder builder = new ConfigurationBuilder();
+                    builder.AddJsonFile(configFile.FilePath);
+                    var configuration = builder.Build();
 
-                var healthReporterMock = new Mock<IHealthReporter>();
+                    using (var pipeline = DiagnosticsPipelineFactory.CreatePipeline(configuration))
+                    {
+                        Assert.NotNull(pipeline);
 
-                var pipeline = DiagnosticsPipelineFactory.CreatePipeline(configuration, healthReporterMock.Object) as DiagnosticsPipeline;
-                Assert.NotNull(pipeline);
+                        Assert.Equal(pipeline.Inputs.Count, 1);
+                        var eventSourceInput = pipeline.Inputs.First() as EventSourceInput;
+                        Assert.NotNull(eventSourceInput);
 
-                healthReporterMock.Verify(o => o.ReportWarning(It.IsAny<string>(), It.IsAny<string>()), Times.Exactly(0));
-                healthReporterMock.Verify(o => o.ReportProblem(It.IsAny<string>(), It.IsAny<string>()), Times.Exactly(0));
+                        var expectedEventSources = new EventSourceConfiguration[2];
+                        expectedEventSources[0] = new EventSourceConfiguration { ProviderName = "Microsoft-ServiceFabric-Services" };
+                        expectedEventSources[1] = new EventSourceConfiguration { ProviderName = "MyCompany-AirTrafficControlApplication-Frontend" };
+                        Assert.True(eventSourceInput.EventSources.SequenceEqual(expectedEventSources));
 
-                Assert.Equal(pipeline.Inputs.Count, 1);
-                var eventSourceInput = pipeline.Inputs.First() as EventSourceInput;
-                Assert.NotNull(eventSourceInput);
+                        Assert.Equal(pipeline.Sinks.Count, 1);
+                        EventSink sink = pipeline.Sinks.First();
 
-                var expectedEventSources = new EventSourceConfiguration[2];
-                expectedEventSources[0] = new EventSourceConfiguration { ProviderName = "Microsoft-ServiceFabric-Services" };
-                expectedEventSources[1] = new EventSourceConfiguration { ProviderName = "MyCompany-AirTrafficControlApplication-Frontend" };
-                Assert.True(eventSourceInput.EventSources.SequenceEqual(expectedEventSources));
+                        var stdSender = sink.Output as StdOutput;
+                        Assert.NotNull(stdSender);
 
-                Assert.Equal(pipeline.Sinks.Count, 1);
-                EventSink sink = pipeline.Sinks.First();
+                        var expectedFilters = new EventMetadataFilter[2];
+                        var metadata = new EventMetadata("importance");
+                        metadata.IncludeCondition = "Level == Verbose";
+                        metadata.Properties.Add("importance", "can be discarded");
+                        expectedFilters[0] = new EventMetadataFilter(metadata);
 
-                var stdSender = sink.Output as StdOutput;
-                Assert.NotNull(stdSender);
+                        metadata = new EventMetadata("metric");
+                        metadata.IncludeCondition = "ProviderName == Microsoft-ServiceFabric-Services && EventName == StatefulRunAsyncFailure";
+                        metadata.Properties.Add("metricName", "StatefulRunAsyncFailure");
+                        metadata.Properties.Add("metricValue", "1.0");
+                        expectedFilters[1] = new EventMetadataFilter(metadata);
 
-                var expectedFilters = new EventMetadataFilter[2];
-                var metadata = new EventMetadata("importance");
-                metadata.IncludeCondition = "Level == Verbose";
-                metadata.Properties.Add("importance", "can be discarded");
-                expectedFilters[0] = new EventMetadataFilter(metadata);
+                        Assert.True(sink.Filters.SequenceEqual(expectedFilters));
+                    }
+                }
+            }
+            finally
+            {
+                TryDeleteFile(CsvHealthReporter.DefaultHealthReportName);
+            }
+        }
 
-                metadata = new EventMetadata("metric");
-                metadata.IncludeCondition = "ProviderName == Microsoft-ServiceFabric-Services && EventName == StatefulRunAsyncFailure";
-                metadata.Properties.Add("metricName", "StatefulRunAsyncFailure");
-                metadata.Properties.Add("metricValue", "1.0");
-                expectedFilters[1] = new EventMetadataFilter(metadata);
-
-                Assert.True(sink.Filters.SequenceEqual(expectedFilters));
+        private static void TryDeleteFile(string fileName)
+        {
+            // Clean up
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
             }
         }
     }
---
+++ test/Microsoft.Diagnostics.EventFlow.Core.Tests/Mocks/HealthReporterMocks.cs
@@ -0,0 +1,37 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Diagnostics.EventFlow.Tests.Mocks
+{
+    public class HealthReporterMock: IHealthReporter
+    {
+        public HealthReporterMock(IConfiguration configuration)
+        {
+
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void ReportHealthy(string description = null, string context = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReportProblem(string description, string context = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReportWarning(string description, string context = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
