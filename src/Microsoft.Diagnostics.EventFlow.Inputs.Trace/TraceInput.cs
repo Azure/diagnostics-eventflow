505df912a93207824afdaeb49a97f38804244fb7
Saar Shen <xiaomi7732@hotmail.com>
2016-08-30 13:12:51 -0700

    Rich the info TraceInput provides

--- src/Microsoft.Diagnostics.EventFlow.Inputs.Trace/TraceInput.cs
+++ src/Microsoft.Diagnostics.EventFlow.Inputs.Trace/TraceInput.cs
@@ -5,14 +5,15 @@
 
 using System;
 using System.Diagnostics;
+using System.Globalization;
+using System.Text;
 using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.Diagnostics.EventFlow.Inputs
 {
-    public class TraceInput : DefaultTraceListener, IObservable<EventData>, IDisposable
+    public class TraceInput : TraceListener, IObservable<EventData>, IDisposable
     {
         public static readonly string TraceTag = nameof(TraceInput);
-
         private SimpleSubject<EventData> subject;
         private readonly IHealthReporter healthReporter;
         public TraceInput(IConfiguration configuration, IHealthReporter healthReporter)
@@ -22,7 +23,6 @@ namespace Microsoft.Diagnostics.EventFlow.Inputs
             Validation.Requires.NotNull(healthReporter, nameof(healthReporter));
 
             this.healthReporter = healthReporter;
-
             this.subject = new SimpleSubject<EventData>();
 
             string traceLevelString = configuration["traceLevel"];
@@ -36,20 +36,136 @@ namespace Microsoft.Diagnostics.EventFlow.Inputs
             this.healthReporter.ReportHealthy($"{nameof(TraceInput)} initialized.", TraceTag);
         }
 
+        #region Overrides for TraceListener
         public override void WriteLine(string message)
+        {
+            SubmitEventData(message, null);
+        }
+
+
+        public override void Fail(string message)
+        {
+            Fail(message, null);
+        }
+
+        public override void Fail(string message, string detailMessage)
+        {
+            if (!string.IsNullOrWhiteSpace(detailMessage))
+            {
+                message = message + Environment.NewLine + detailMessage;
+            }
+            SubmitEventData(message, SourceLevels.Error.ToString());
+        }
+
+        public override void TraceData(TraceEventCache eventCache, string source, TraceEventType eventType, int id, object data)
+        {
+            if (this.Filter != null && !this.Filter.ShouldTrace(eventCache, source, eventType, id, null, null, data, null))
+            {
+                return;
+            }
+            string message = string.Empty;
+            if (data != null)
+            {
+                message = data.ToString();
+            }
+            SubmitEventData(message, eventType.ToString(), id, source);
+        }
+
+        public override void TraceData(TraceEventCache eventCache, string source, TraceEventType eventType, int id, params object[] data)
+        {
+            if (this.Filter != null && !this.Filter.ShouldTrace(eventCache, source, eventType, id, null, null, null, data))
+            {
+                return;
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            if (data != null)
+            {
+                for (int i = 0; i < data.Length; i++)
+                {
+                    if (i != 0)
+                    {
+                        stringBuilder.Append(", ");
+                    }
+                    if (data[i] != null)
+                    {
+                        stringBuilder.Append(data[i].ToString());
+                    }
+                }
+            }
+            SubmitEventData(stringBuilder.ToString(), eventType.ToString(), id, source);
+        }
+
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string message)
+        {
+            if (this.Filter != null && !this.Filter.ShouldTrace(eventCache, source, eventType, id, message, null, null, null))
+            {
+                return;
+            }
+            SubmitEventData(message, eventType.ToString(), id, source);
+        }
+
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string format, params object[] args)
+        {
+            if (this.Filter != null && !this.Filter.ShouldTrace(eventCache, source, eventType, id, format, args, null, null))
+            {
+                return;
+            }
+            string message = null;
+            if (args != null)
+            {
+                message = string.Format(CultureInfo.InvariantCulture, format, args);
+            }
+            else
+            {
+                message = format;
+            }
+            SubmitEventData(message, eventType.ToString(), id, source);
+        }
+
+#if !NETSTANDARD1_6
+        public override void TraceTransfer(TraceEventCache eventCache, string source, int id, string message, Guid relatedActivityId)
+        {
+            SubmitEventData(message, TraceEventType.Transfer.ToString(), id, source, relatedActivityId.ToString());
+        }
+#endif
+        #endregion
+
+        private void SubmitEventData(string message, string level, int? id = null, string source = null, string relatedActivityId = null)
         {
             try
             {
-                EventData data = new EventData()
+                EventData eventEntry = new EventData();
+                eventEntry.Message = message;
+
+                if (!string.IsNullOrEmpty(level))
                 {
-                    ProviderName = nameof(TraceInput),
-                    Message = message
-                };
-                this.subject.OnNext(data);
+                    eventEntry.Level = level;
+                }
+
+                if (id != null && id.HasValue)
+                {
+                    eventEntry.EventId = id.Value;
+                }
+
+                if (!string.IsNullOrEmpty(source))
+                {
+                    eventEntry.ProviderName = source;
+                }
+                else
+                {
+                    eventEntry.ProviderName = TraceTag;
+                }
+
+                if (!string.IsNullOrWhiteSpace(relatedActivityId))
+                {
+                    eventEntry.ActivityID = relatedActivityId;
+                }
+
+                this.subject.OnNext(eventEntry);
             }
             catch (Exception ex)
             {
-                this.healthReporter.ReportProblem($"Fail to write message. Error: {ex.ToString()}", TraceTag);
+                ReportException(ex);
             }
         }
 
@@ -62,6 +178,16 @@ namespace Microsoft.Diagnostics.EventFlow.Inputs
         {
             base.Dispose(disposing);
             this.subject.Dispose();
+        }
+
+        public override void Write(string message)
+        {
+            WriteLine(message);
+        }
+
+        private void ReportException(Exception ex)
+        {
+            this.healthReporter?.ReportProblem($"Fail to write message. Error: {ex.ToString()}", TraceTag);
         }
     }
 }
