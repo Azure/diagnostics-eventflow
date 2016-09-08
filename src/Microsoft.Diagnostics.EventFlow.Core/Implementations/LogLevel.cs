8061ed3777734a291d2f724566a440264f18f951
Karol Zadora-Przylecki <karolz@microsoft.com>
2016-09-08 09:09:48 -0700

    Add missing file

---
+++ src/Microsoft.Diagnostics.EventFlow.Core/Implementations/LogLevel.cs
@@ -0,0 +1,37 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace Microsoft.Diagnostics.EventFlow
+{
+
+    public enum LogLevel
+    {
+        Critical = 1,
+        Error = 2,
+        Warning = 3,
+        Informational = 4,
+        Verbose = 5
+    }
+
+    public static class EventLevelExtensions
+    {
+        // Micro-optimization: Enum.ToString() uses type information and does a binary search for the value,
+        // which is kind of slow. We are going to to the conversion manually instead.
+        private static readonly string[] LogLevelNames = new string[]
+        {
+            "NotUsed",
+            "Critical",
+            "Error",
+            "Warning",
+            "Informational",
+            "Verbose"
+        };
+
+        public static string GetName(this LogLevel level)
+        {
+            return LogLevelNames[(int) level];
+        }
+    }
+}
