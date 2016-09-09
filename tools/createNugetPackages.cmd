cbc807dfa9d6c5d319638668a61fc809a89068f1
Saar Shen <saars@microsoft.com>
2016-09-09 19:14:54 +0000

    Add a nuget spec for bootstrap usage

--- tools/createNugetPackages.cmd
+++ tools/createNugetPackages.cmd
@@ -6,10 +6,12 @@ popd
 SET SUFFIX=%1
 IF '%SUFFIX%' == '' GOTO NOSUFFIX
 for /f %%a in ('findstr /sm packOptions %parentDir%\src\project.json') do dotnet pack %%a --no-build -c Release -o "%parentDir%\nugets" --version-suffix %SUFFIX%
+"%~dp0\nuget.exe" pack "%parentDir%\nuspecs\Suite\Microsoft.Diagnostics.EventFlow.Suite.nuspec" -OutputDirectory "%parentDir%\nugets" -Suffix %SUFFIX%
 GOTO END
 
 :NOSUFFIX
 for /f %%a in ('findstr /sm packOptions %parentDir%\src\project.json') do dotnet pack %%a --no-build -c Release -o "%parentDir%\nugets"
+"%~dp0\nuget.exe" pack "%parentDir%\nuspecs\Suite\Microsoft.Diagnostics.EventFlow.Suite.nuspec" -OutputDirectory "%parentDir%\nugets"
 GOTO END
 
 :END
