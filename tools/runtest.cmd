be7c8d3882a7d34dd1740e78b946be0d10628745
Yanming Tang <yantang@microsoft.com>
2016-09-15 22:07:57 +0000

    PR 36666: Fix runtest.cmd. So the script will fail if the unit tests failed
     - Fix runtest.cmd. So the script will fail if the unit tests failed
    The %errorlevel% will only remember the last execution of the for loop. So if some unit test failed in the first test project, the build will still succeed.

--- tools/runtest.cmd
+++ tools/runtest.cmd
@@ -1,2 +1,4 @@
 @ECHO OFF
-for /f %%a in ('dir %~dp0..\test\project.json /b /s ^| find /i "Tests"') do dotnet test %%a
+SET failed=0
+for /f %%a in ('dir %~dp0..\test\project.json /b /s ^| find /i "Tests"') do dotnet test %%a || set failed=1
+EXIT %failed%
