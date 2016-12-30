ecd5779e551a4d51f4a9422924a1d6f7f079e288
Karol Zadora-Przylecki <karolz@microsoft.com>
2016-12-29 16:38:56 -0800

    Fix newly added DiagnosticsPipelineTest
    Apparently EventHub client requires realistic connection string

--- test/Microsoft.Diagnostics.EventFlow.Core.Tests/DiagnosticsPipelineFactoryTests.cs
+++ test/Microsoft.Diagnostics.EventFlow.Core.Tests/DiagnosticsPipelineFactoryTests.cs
@@ -359,7 +359,7 @@ namespace Microsoft.Diagnostics.EventFlow.Core.Tests
                         }, 
                         {
                             ""type"": ""EventHub"",
-                            ""connectionString"": ""unused""
+                            ""connectionString"": ""Endpoint=sb://unused.servicebus.windows.net/;SharedAccessKeyName=send-only;SharedAccessKey=+lw95uDEcOLYE/zZFycZx3gxgomPgzfFmSdj+iBQiI8=;EntityPath=hub1""
                         }, 
                         {
                             ""type"": ""ApplicationInsights"",
