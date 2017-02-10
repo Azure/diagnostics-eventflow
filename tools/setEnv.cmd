5e8c18e24f63a2714bfd4f7924627a0aa444f279
Karol Zadora-Przylecki <karolz@microsoft.com>
2017-02-10 14:13:07 -0800

    Ensure correct version of .NET SDK is installed on the build machine

--- tools/setEnv.cmd
+++ tools/setEnv.cmd
@@ -1,8 +1,7 @@
 @ECHO OFF
 pushd %~dp0
 
-where dotnet >NUL 2>NUL
-IF %ERRORLEVEL% NEQ 0 powershell.exe -File InstallCoreSDK.ps1
+powershell.exe -File InstallCoreSDK.ps1
 
 ECHO %PATH% | findstr /I /C:"C:\Program Files\dotnet" >NUL 2>NUL
 IF %ERRORLEVEL% NEQ 0 SET PATH=%PATH%;C:\Program Files\dotnet\
