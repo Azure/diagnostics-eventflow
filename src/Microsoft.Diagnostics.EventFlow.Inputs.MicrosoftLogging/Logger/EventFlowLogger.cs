233fc815de3553f417d5087386bd7cec61a02d30
Morten Fischer-Madsen <MFM@fischerkerrn.com>
2017-07-12 22:03:43 +0200

    Removed unused using statement

--- src/Microsoft.Diagnostics.EventFlow.Inputs.MicrosoftLogging/Logger/EventFlowLogger.cs
+++ src/Microsoft.Diagnostics.EventFlow.Inputs.MicrosoftLogging/Logger/EventFlowLogger.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Internal;
 
