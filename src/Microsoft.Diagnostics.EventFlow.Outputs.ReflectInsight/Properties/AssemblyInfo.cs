dcd5a4ed662f63351b5ce71ffeed0c3b9ab941d7
codematrix <ross_pellegrino@hotmail.com>
2017-07-18 21:29:07 -0400

    Adding ReflectInsightInsightsOutput to Solution

---
+++ src/Microsoft.Diagnostics.EventFlow.Outputs.ReflectInsight/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Microsoft.Diagnostics.EventFlow.Outputs.ReflectInsight")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1AA9E987-1AC9-469A-9B16-98215C96E530")]
