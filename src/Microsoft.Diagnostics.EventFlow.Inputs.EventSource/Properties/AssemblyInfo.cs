2668e1b802655467ad10f05e0f462f960cb6559f
Karol Zadora-Przylecki <karolz@microsoft.com>
2018-03-26 14:46:22 -0700

    Do not delay sign EventFlow assemblies

--- src/Microsoft.Diagnostics.EventFlow.Inputs.EventSource/Properties/AssemblyInfo.cs
+++ src/Microsoft.Diagnostics.EventFlow.Inputs.EventSource/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("705eb0a8-8fb0-4f80-8f6a-7bb71a202a2a")]
 
-[assembly: InternalsVisibleTo("Microsoft.Diagnostics.EventFlow.Inputs.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010007d1fa57c4aed9f0a32e84aa0faefd0de9e8fd6aec8f87fb03766c834c99921eb23be79ad9d5dcc1dd9ad236132102900b723cf980957fc4e177108fc607774f29e8320e92ea05ece4e821c0a5efe8f1645c4c0c93c1ab99285d622caa652c1dfad63d745d6f2de5f17e5eaf0fc4963d261c8a12436518206dc093344d5ad293")]
+[assembly: InternalsVisibleTo("Microsoft.Diagnostics.EventFlow.Inputs.Tests")]
 
