52dd753cfb2821c9338a4232c7d2e7b0e319071f
Ted van der Veen <ntvanderveen@stateofcrowd.com>
2018-04-23 19:45:23 +0200

    Undo changes to EventHubOutputFactory

--- src/Microsoft.Diagnostics.EventFlow.Outputs.EventHub/EventHubFactory.cs
+++ src/Microsoft.Diagnostics.EventFlow.Outputs.EventHub/EventHubFactory.cs
@@ -3,30 +3,19 @@
 //  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
 // ------------------------------------------------------------
 
-using Microsoft.Diagnostics.EventFlow.Metadata;
-using Microsoft.Diagnostics.EventFlow.Outputs.EventHub;
 using Microsoft.Extensions.Configuration;
 using Validation;
 
 namespace Microsoft.Diagnostics.EventFlow.Outputs
 {
-    public class EventHubOutputFactory : IPipelineItemFactory<IOutput>
+    public class EventHubOutputFactory : IPipelineItemFactory<EventHubOutput>
     {
-        public IOutput CreateItem(IConfiguration configuration, IHealthReporter healthReporter)
+        public EventHubOutput CreateItem(IConfiguration configuration, IHealthReporter healthReporter)
         {
             Requires.NotNull(configuration, nameof(configuration));
             Requires.NotNull(healthReporter, nameof(healthReporter));
 
-            //Check wether the optional partitionKeyProperty configuration element is set. If so, return an instance of PartitionedEventHubOutput
-            string partitioKeyProperty = configuration[PartitionKeyData.PartitionKeyPropertyMoniker];
-            if (string.IsNullOrWhiteSpace(partitioKeyProperty) == false)
-            {
-                return new PartitionedEventHubOutput(configuration, healthReporter);
-            }
-            else //return output handler that does not use Event Hub partinions
-            {
-                return new EventHubOutput(configuration, healthReporter);
-            }
+            return new EventHubOutput(configuration, healthReporter);
         }
     }
 }
