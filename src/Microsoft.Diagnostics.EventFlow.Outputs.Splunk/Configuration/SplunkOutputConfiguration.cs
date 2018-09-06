fd9a974bdf7f6b293934f85c48cd5411c7b0ee7b
hortha <andrew@horth.co.uk>
2018-09-06 14:37:48 +0100

    Added Splunk HTTP Event Collector Output

---
+++ src/Microsoft.Diagnostics.EventFlow.Outputs.Splunk/Configuration/SplunkOutputConfiguration.cs
@@ -0,0 +1,39 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using Microsoft.Diagnostics.EventFlow.Configuration;
+
+namespace Microsoft.Diagnostics.EventFlow.Outputs.Splunk.Configuration
+{
+    public class SplunkOutputConfiguration : ItemConfiguration
+    {
+        public string ServiceBaseAddress { get; set; }
+
+        public string AuthenticationToken { get; set; }
+
+        public string Host { get; set; }
+
+        public string Index { get; set; }
+        
+        public string Source { get; set; }
+
+        public string SourceType { get; set; }
+
+        public SplunkOutputConfiguration DeepClone()
+        {
+            var other = new SplunkOutputConfiguration()
+            {
+                ServiceBaseAddress =  this.ServiceBaseAddress,
+                AuthenticationToken = this.AuthenticationToken,
+                Host = this.Host,
+                Index =  this.Index,
+                Source = this.Source,
+                SourceType = this.SourceType
+            };
+
+            return other;
+        }
+    }
+}
---
+++ src/Microsoft.Diagnostics.EventFlow.Outputs.Splunk/HttpExponentialRetryMessageHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Diagnostics.EventFlow.Outputs.Splunk
+{
+    /// <summary>
+    /// Implements a message handler that employs exponential backoff retry policy for HTTP requests
+    /// </summary>
+    internal class HttpExponentialRetryMessageHandler : DelegatingHandler
+    {
+        private const int MaxRetries = 3;
+        private readonly TimeSpan[] AttemptDelays = new[] { TimeSpan.FromMilliseconds(500), TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(5) };        
+
+        public HttpExponentialRetryMessageHandler() : base(new HttpClientHandler())
+        {            
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {            
+            int attempt = 0;
+
+            while (true)
+            {
+                var response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+                if (attempt == MaxRetries || (int)response.StatusCode < 500)
+                {
+                    return response;
+                }
+                else
+                {
+                    await Task.Delay(AttemptDelays[attempt++], cancellationToken).ConfigureAwait(false);
+                }
+            }
+        }
+    }
+}
