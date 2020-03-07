74e3852405b530fcbd190f6593c9ebd5fbee8861
Karol Zadora-Przylecki <karolz@microsoft.com>
2020-03-06 18:23:37 -0800

    Default JSON serialization settings test

---
+++ test/Microsoft.Diagnostics.EventFlow.Core.Tests/JsonSerializationTests.cs
@@ -0,0 +1,33 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using Newtonsoft.Json;
+using Xunit;
+
+using Microsoft.Diagnostics.EventFlow.TestHelpers;
+
+namespace Microsoft.Diagnostics.EventFlow.Core.Tests
+{
+    public class JsonSerializationTests
+    {
+        [Fact]
+        public void SerializesMethodInfoAsFullyQualifiedName()
+        {
+            var obj = new
+            {
+                Name = "foo",
+                Method = typeof(JsonSerializationTests).GetMethod(nameof(SerializesMethodInfoAsFullyQualifiedName))
+            };
+
+            string objString = JsonConvert.SerializeObject(obj, EventFlowJsonUtilities.GetDefaultSerializerSettings());
+
+            Assert.Equal(
+            @"{
+                ""Name"":""foo"",
+                ""Method"":""Microsoft.Diagnostics.EventFlow.Core.Tests.JsonSerializationTests.SerializesMethodInfoAsFullyQualifiedName""
+            }".RemoveAllWhitespace(), objString);
+        }
+    }
+}
