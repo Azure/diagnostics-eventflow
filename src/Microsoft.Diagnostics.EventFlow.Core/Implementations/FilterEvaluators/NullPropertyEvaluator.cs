754ca3888c1a5734df6ce68d8c1d8e43af080642
rw-ksmith <kevin.smith@renweb.com>
2020-08-24 17:05:50 -0500

    add nullpropertyevaluator

---
+++ src/Microsoft.Diagnostics.EventFlow.Core/Implementations/FilterEvaluators/NullPropertyEvaluator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Microsoft.Diagnostics.EventFlow.FilterEvaluators
+{
+    internal class NullPropertyEvaluator : EventPropertyExpressionEvaluator
+    {
+        public NullPropertyEvaluator(string propertyName, string value) : base(propertyName, value)
+        {
+        }
+
+        public override string SemanticsString
+        {
+            get { return string.Format(CultureInfo.InvariantCulture, "(__EqualityEvaluator:{0} is {1})", this.propertyName, this.value); }
+        }
+
+        public override bool Evaluate(EventData e)
+        {
+            bool ignored;
+            return EvaluateEquality(e, out ignored);
+        }
+
+        protected bool EvaluateEquality(EventData e, out bool fullyEvaluated)
+        {
+            if (e == null)
+            {
+                throw new ArgumentNullException(nameof(e));
+            }
+
+            fullyEvaluated = true;
+
+            if (!e.TryGetPropertyValue(this.propertyName, out _))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
