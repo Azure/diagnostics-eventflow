35fd1e2ee0811ff8cd05104aea53a9c9d194aa09
Karol Zadora-Przylecki <karolz@microsoft.com>
2020-10-11 17:17:31 -0700

    Add connection string support for AppInsights output configuration
    Addresses [issue 369](https://github.com/Azure/diagnostics-eventflow/issues/369)

--- test/Microsoft.Diagnostics.EventFlow.Outputs.Tests/ApplicationInsightsOutputTests.cs
+++ test/Microsoft.Diagnostics.EventFlow.Outputs.Tests/ApplicationInsightsOutputTests.cs
@@ -6,12 +6,13 @@
 using Moq;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Text.RegularExpressions;
 using Xunit;
 
 using Microsoft.Diagnostics.EventFlow.Configuration;
 using Microsoft.ApplicationInsights.DataContracts;
+
+using Microsoft.Diagnostics.EventFlow.TestHelpers;
 
 namespace Microsoft.Diagnostics.EventFlow.Outputs.Tests
 {
@@ -38,9 +39,78 @@ namespace Microsoft.Diagnostics.EventFlow.Outputs.Tests
             Assert.Matches(dateTimeOffsetRegex, propertyBag.Properties["DateTimeOffsetProperty"]);
         }
 
+        [Fact]
+        public void InstrumentationKeyOverridesConnectionString()
+        {
+            var healthReporterMock = new Mock<IHealthReporter>();
+            var config = new ApplicationInsightsOutputConfiguration();
+            config.ConnectionString = "InstrumentationKey=d0198460-ce4a-4efa-9e17-3edef2b40f15";
+            config.InstrumentationKey = "c8da242c-9f2d-45ab-913c-c9953516e9c2";
+            var output = new ApplicationInsightsOutput(config, healthReporterMock.Object);
+
+            VerifyNoErrorsOrWarnings(healthReporterMock);
+
+            var telemetry = new TraceTelemetry();
+            output.telemetryClient.InitializeInstrumentationKey(telemetry);
+            Assert.Equal("c8da242c-9f2d-45ab-913c-c9953516e9c2", telemetry.Context.InstrumentationKey, StringComparer.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public void ConnectionStringOverridesConfigurationFile()
+        {
+            const string aiConfiguration = @"
+                <?xml version=""1.0"" encoding=""utf-8""?>
+                <ApplicationInsights xmlns=""http://schemas.microsoft.com/ApplicationInsights/2013/Settings"">
+                    <InstrumentationKey>c8da242c-9f2d-45ab-913c-c9953516e9c2</InstrumentationKey>
+                    <TelemetryChannel Type=""Microsoft.ApplicationInsights.WindowsServer.TelemetryChannel.ServerTelemetryChannel, Microsoft.AI.ServerTelemetryChannel""/>
+                </ApplicationInsights>
+            ";
+
+            var healthReporterMock = new Mock<IHealthReporter>();
+            var telemetry = new TraceTelemetry();
+
+            using (var configFile = new TemporaryFile())
+            {
+                configFile.Write(aiConfiguration);
+                var config = new ApplicationInsightsOutputConfiguration();
+                config.ConnectionString = "InstrumentationKey=d0198460-ce4a-4efa-9e17-3edef2b40f15";
+                config.ConfigurationFilePath = configFile.FilePath;
+
+                var output = new ApplicationInsightsOutput(config, healthReporterMock.Object);
+
+                VerifyNoErrorsOrWarnings(healthReporterMock);
+                
+                output.telemetryClient.InitializeInstrumentationKey(telemetry);
+            }
+
+            Assert.Equal("d0198460-ce4a-4efa-9e17-3edef2b40f15", telemetry.Context.InstrumentationKey, StringComparer.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public void InvalidConfigurationResultsInAnError()
+        {
+            var healthReporterMock = new Mock<IHealthReporter>();
+            var config = new ApplicationInsightsOutputConfiguration();
+            var output = new ApplicationInsightsOutput(config, healthReporterMock.Object);
+
+            healthReporterMock.Verify(
+                hr => hr.ReportProblem(
+                    It.IsRegex("invalid configuration"), 
+                    It.Is<string>(ctx => string.Equals(ctx, EventFlowContextIdentifiers.Output, StringComparison.Ordinal))),
+                Times.Exactly(1));
+        }
+
         private class PropertyBag: ISupportProperties
         {
             public IDictionary<string, string> Properties { get; } = new Dictionary<string, string>();
+        }
+
+        private void VerifyNoErrorsOrWarnings(Mock<IHealthReporter> healthReporterMock)
+        {
+            healthReporterMock.Verify(hr => hr.ReportWarning(It.IsAny<string>(), It.IsAny<string>()),
+                    Times.Exactly(0));
+            healthReporterMock.Verify(hr => hr.ReportProblem(It.IsAny<string>(), It.IsAny<string>()),
+                Times.Exactly(0));
         }
     }
 }
