6ce51fae6b116699ebc5bd26b3ba586f6f8030e6
Karol Zadora-Przylecki <karolz@microsoft.com>
2020-12-26 11:30:31 -0800

    Build .NET 4.5.2 binaries instead of .NET 4.5.1
    .NET 4.5.1 has been out of support since January 2016

--- src/Microsoft.Diagnostics.EventFlow.Core/Implementations/JsonConverters/IPEndpointConverter.cs
+++ src/Microsoft.Diagnostics.EventFlow.Core/Implementations/JsonConverters/IPEndpointConverter.cs
@@ -5,7 +5,7 @@
 
 using System;
 using System.Net;
-#if NET451 || NETSTANDARD1_6
+#if NET452 || NETSTANDARD1_6
 using System.Reflection;
 #endif
 using Newtonsoft.Json;
@@ -16,7 +16,7 @@ namespace Microsoft.Diagnostics.EventFlow.JsonConverters
     {
         public override bool CanConvert(Type objectType)
         {
-#if NET451 || NETSTANDARD1_6
+#if NET452 || NETSTANDARD1_6
             return typeof(EndPoint).GetTypeInfo().IsAssignableFrom(objectType);
 #else
             return typeof(EndPoint).IsAssignableFrom(objectType);
