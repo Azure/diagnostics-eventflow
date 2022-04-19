89e7430ac0590ccaf81d6ff6e042d85a3be902d1
Karol Zadora-Przylecki <karolz@microsoft.com>
2022-04-19 16:38:20 -0700

    Don't attempt to restore deleted project

--- tools/restore.cmd
+++ tools/restore.cmd
@@ -1,7 +1,6 @@
 @ECHO OFF
 dotnet restore %~dp0..\
 pushd %~dp0..
-tools\nuget.exe restore "src\Microsoft.Diagnostics.EventFlow.FilterParserGenerator\Microsoft.Diagnostics.EventFlow.FilterParserGenerator.csproj" -PackagesDirectory packages
 tools\nuget.exe restore "src\Microsoft.Diagnostics.EventFlow.Signing\Microsoft.Diagnostics.EventFlow.Signing.csproj" -PackagesDirectory packages
 tools\nuget.exe restore "src\Microsoft.Diagnostics.EventFlow.NugetSigning\Microsoft.Diagnostics.EventFlow.NugetSigning.csproj" -PackagesDirectory packages
 popd
