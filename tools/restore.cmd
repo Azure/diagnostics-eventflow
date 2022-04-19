f51b038be64a5b48d6aa7a2ec7c6596501f8cdf6
Karol Zadora-Przylecki <karolz@microsoft.com>
2022-04-19 16:35:51 -0700

    Use Pegasus directly

--- tools/restore.cmd
+++ tools/restore.cmd
@@ -4,5 +4,4 @@ pushd %~dp0..
 tools\nuget.exe restore "src\Microsoft.Diagnostics.EventFlow.FilterParserGenerator\Microsoft.Diagnostics.EventFlow.FilterParserGenerator.csproj" -PackagesDirectory packages
 tools\nuget.exe restore "src\Microsoft.Diagnostics.EventFlow.Signing\Microsoft.Diagnostics.EventFlow.Signing.csproj" -PackagesDirectory packages
 tools\nuget.exe restore "src\Microsoft.Diagnostics.EventFlow.NugetSigning\Microsoft.Diagnostics.EventFlow.NugetSigning.csproj" -PackagesDirectory packages
-dotnet build src\Microsoft.Diagnostics.EventFlow.FilterParserGenerator\Microsoft.Diagnostics.EventFlow.FilterParserGenerator.csproj /t:CompilePegGrammars
 popd
